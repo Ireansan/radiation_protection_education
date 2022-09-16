dba1561195cf6753e154d646775a40787d145723
Ireansan <kkntp528@icloud.com>
2022-09-16 17:32:04 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   basePath: process.env.GITHUB_ACTIONS && '/radiation_protection_education',
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 module.exports = nextConfig
