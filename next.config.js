1b8e1ff4363e9e35753a074c518d8b535ffad924
Ireansan <kkntp528@icloud.com>
2022-09-16 13:19:11 +0900

    fix css and setup css

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   basePath: process.env.GITHUB_ACTIONS && '/radiation_protection_education',
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 module.exports = nextConfig
