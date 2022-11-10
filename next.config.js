8c4cc99a2103f2fff6be874e3966cf15475006f4
Ireansan <kkntp528@icloud.com>
2022-11-10 23:58:23 +0900

    test

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   assetPrefix: process.env.NODE_ENV === "development" ? '' : '/radiation_protection_education',
   reactStrictMode: true,
   trailingSlash: true,
+  ignoreDuringBuilds: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
 }
 
 module.exports = nextConfig
