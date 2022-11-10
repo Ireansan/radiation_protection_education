117b31fd6e30df6374c9d1a6200588658c0baef7
Ireansan <kkntp528@icloud.com>
2022-11-10 14:58:21 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.GITHUB_ACTIONS ? '/radiation_protection_education' : '',
-  assetPrefix: process.env.GITHUB_ACTIONS ? '/radiation_protection_education/' : '',
+  assetPrefix: '/radiation_protection_education/',
   trailingSlash: true,
   reactStrictMode: true,
 }
