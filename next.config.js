aa8de4d85d9998a5515f0ff1b6c2553b1935d6b1
Ireansan <kkntp528@icloud.com>
2022-11-10 20:05:48 +0900

    update nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NODE_ENV === "development" ? '' : '/radiation_protection_education',
+  assetPrefix: process.env.NODE_ENV === "development" ? '' : '/radiation_protection_education',
   trailingSlash: true,
   reactStrictMode: true,
 }
