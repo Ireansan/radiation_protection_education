4f3afe6945b84e95833dc85415e9a6d5db35af3c
Ireansan <kkntp528@icloud.com>
2022-11-28 13:26:04 +0900

    animation test ver

--- components/models/Dose/Dose_all_Animation.tsx
+++ components/models/Dose/Dose_all_Animation.tsx
@@ -1,17 +1,60 @@
 import * as THREE from "three";
-import React from "react";
-import { extend } from "@react-three/fiber";
+import React, { useRef, useMemo, useEffect } from "react";
+import { extend, useFrame } from "@react-three/fiber";
+import { useAnimations } from "@react-three/drei";
 
 import * as DOSE_ALL from "./Dose_all";
-import { VolumeGroup } from "../../volumeRender";
-extend({ VolumeGroup });
+import { VolumeGroup, VolumeAnimationObject } from "../../volumeRender";
+extend({ VolumeGroup, VolumeAnimationObject });
 
 export function Dose_all_Animation({
     ...props
-}: JSX.IntrinsicElements["volumeGroup"]) {
+}: JSX.IntrinsicElements["volumeAnimationObject"]) {
+    const group = useRef<VolumeAnimationObject>(null!);
+
+    /**
+     * @link https://github.com/pmndrs/drei/blob/cce70ae77b5151601089114259fbffab8747c8fa/src/core/useAnimations.tsx
+     */
+    const [actualRef] = React.useState(() =>
+        group
+            ? group instanceof THREE.Object3D
+                ? { current: group }
+                : group
+            : group
+    );
+    const [mixer] = React.useState(
+        () => new THREE.AnimationMixer(undefined as unknown as THREE.Object3D)
+    );
+    const [actions, setActions] = React.useState(() => {
+        const actions = {} as {
+            [key in THREE.AnimationClip["name"]]: THREE.AnimationAction | null;
+        };
+
+        return actions;
+    });
+    const lazyActions = React.useRef<{
+        [key: string]: THREE.AnimationAction | null;
+    }>({});
+
+    useEffect(() => {
+        group.current.animations.forEach((clip) => {
+            lazyActions.current[clip.name] = mixer.clipAction(
+                clip,
+                actualRef.current
+            );
+        });
+        setActions(lazyActions.current);
+    }, [group]);
+
+    useEffect(() => {
+        actions["volumeAnimation"]?.reset().play();
+    }, [actions]);
+
+    useFrame((state, delta) => mixer.update(delta * 16));
+
     return (
         <>
-            <volumeGroup isAnimationGroup={true} {...props}>
+            <volumeAnimationObject ref={group} {...props}>
                 <DOSE_ALL.Dose_all_1 />
                 <DOSE_ALL.Dose_all_2 />
                 <DOSE_ALL.Dose_all_3 />
@@ -28,7 +71,7 @@ export function Dose_all_Animation({
                 <DOSE_ALL.Dose_all_14 />
                 <DOSE_ALL.Dose_all_15 />
                 <DOSE_ALL.Dose_all_16 />
-            </volumeGroup>
+            </volumeAnimationObject>
         </>
     );
 }
