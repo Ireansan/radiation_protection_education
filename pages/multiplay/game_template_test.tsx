eb42d0355376c51e9969849093c147ab0ebba24a
Ireansan <kkntp528@icloud.com>
2023-01-19 11:15:35 +0900

    update css

--- pages/multiplay/game_template_test.tsx
+++ pages/multiplay/game_template_test.tsx
@@ -1,12 +1,5 @@
 import { GameTemplate } from "../../components/game_template";
-import styles from "../../styles/threejs.module.css";
 
 export default function App() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.canvas}>
-                <GameTemplate />
-            </div>
-        </div>
-    );
+    return <GameTemplate />;
 }
