6281a4add8c00be02bfd575759c5f8ef807de184
Ireansan <kkntp528@icloud.com>
2023-03-02 11:34:42 +0900

    change struct for new controls

--- components/models/Dose/Dose_all_Animation.tsx
+++ components/models/Dose/Dose_all_Animation.tsx
@@ -4,10 +4,7 @@ import { extend, ReactThreeFiber } from "@react-three/fiber";
 import { useAnimations } from "@react-three/drei";
 
 import * as DOSE_ALL from "./Dose_all";
-import {
-    VolumeAnimationObject,
-    VolumeAnimationGroup,
-} from "../../volumeRender";
+import { VolumeAnimationObject } from "../../volumeRender";
 extend({ VolumeAnimationObject });
 
 export function Dose_all_Animation({
@@ -15,24 +12,22 @@ export function Dose_all_Animation({
 }: JSX.IntrinsicElements["volumeAnimationObject"]) {
     return (
         <>
-            <VolumeAnimationGroup {...props}>
-                <DOSE_ALL.Dose_all_1 />
-                <DOSE_ALL.Dose_all_2 />
-                <DOSE_ALL.Dose_all_3 />
-                <DOSE_ALL.Dose_all_4 />
-                <DOSE_ALL.Dose_all_5 />
-                <DOSE_ALL.Dose_all_6 />
-                <DOSE_ALL.Dose_all_7 />
-                <DOSE_ALL.Dose_all_8 />
-                <DOSE_ALL.Dose_all_9 />
-                <DOSE_ALL.Dose_all_10 />
-                <DOSE_ALL.Dose_all_11 />
-                <DOSE_ALL.Dose_all_12 />
-                <DOSE_ALL.Dose_all_13 />
-                <DOSE_ALL.Dose_all_14 />
-                <DOSE_ALL.Dose_all_15 />
-                <DOSE_ALL.Dose_all_16 />
-            </VolumeAnimationGroup>
+            <DOSE_ALL.Dose_all_1 />
+            <DOSE_ALL.Dose_all_2 />
+            <DOSE_ALL.Dose_all_3 />
+            <DOSE_ALL.Dose_all_4 />
+            <DOSE_ALL.Dose_all_5 />
+            <DOSE_ALL.Dose_all_6 />
+            <DOSE_ALL.Dose_all_7 />
+            <DOSE_ALL.Dose_all_8 />
+            <DOSE_ALL.Dose_all_9 />
+            <DOSE_ALL.Dose_all_10 />
+            <DOSE_ALL.Dose_all_11 />
+            <DOSE_ALL.Dose_all_12 />
+            <DOSE_ALL.Dose_all_13 />
+            <DOSE_ALL.Dose_all_14 />
+            <DOSE_ALL.Dose_all_15 />
+            <DOSE_ALL.Dose_all_16 />
         </>
     );
 }
--- components/models/Dose_nocurtain/Dose_nocurtain_all_Animation.tsx
+++ components/models/Dose_nocurtain/Dose_nocurtain_all_Animation.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { extend } from "@react-three/fiber";
 
 import * as DOSE_NOCURTAIN_ALL from "./Dose_nocurtain_all";
-import {
-    VolumeAnimationObject,
-    VolumeAnimationGroup,
-} from "../../volumeRender";
+import { VolumeAnimationObject } from "../../volumeRender";
 extend({ VolumeAnimationObject });
 
 export function Dose_nocurtain_all_Animation({
@@ -13,24 +10,22 @@ export function Dose_nocurtain_all_Animation({
 }: JSX.IntrinsicElements["volumeAnimationObject"]) {
     return (
         <>
-            <VolumeAnimationGroup {...props}>
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_1 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_2 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_3 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_4 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_5 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_6 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_7 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_8 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_9 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_10 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_11 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_12 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_13 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_14 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_15 />
-                <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_16 />
-            </VolumeAnimationGroup>
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_1 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_2 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_3 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_4 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_5 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_6 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_7 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_8 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_9 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_10 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_11 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_12 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_13 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_14 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_15 />
+            <DOSE_NOCURTAIN_ALL.Dose_nocurtain_all_16 />
         </>
     );
 }
