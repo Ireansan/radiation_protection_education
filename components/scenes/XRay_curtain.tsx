f2f34794a7d2fe8b1351fa0141da36526f45a47b
Ireansan <kkntp528@icloud.com>
2023-03-13 15:04:35 +0900

    add new scene

---
+++ components/scenes/XRay_curtain.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, forwardRef } from "react";
+
+import { Canvas, extend, ReactThreeFiber } from "@react-three/fiber";
+import {
+    OrthographicCamera,
+    OrbitControls,
+    Stats,
+    GizmoHelper,
+    GizmoViewport,
+    Box,
+    Sphere,
+} from "@react-three/drei";
+import * as THREE from "three";
+
+import {
+    VolumeAnimationObject,
+    VolumeGroup,
+    VolumeAnimationControls,
+    VolumeClippingControls,
+    VolumeParameterControls,
+} from "../volumeRender";
+extend({ VolumeGroup });
+
+import { VolumeCompareControls } from "../volumeRender/VolumeCompareControls";
+
+import * as MODELS from "../models";
+
+function XRayCurtain() {
+    // FIXME:
+    const ref = useRef<VolumeGroup>(null!);
+    const refCurtain = useRef<VolumeGroup>(null);
+    const refCurtainAnimation = useRef<VolumeAnimationObject>(null);
+    const refNocurtain = useRef<VolumeGroup>(null);
+    const refNocurtainAnimation = useRef<VolumeAnimationObject>(null);
+
+    useEffect(() => {
+        console.log(ref.current);
+    }, [ref]);
+
+    return (
+        <>
+            <Canvas camera={{ position: [32, 64, 32] }}>
+                {/* Volume Objects */}
+                <volumeGroup ref={ref}>
+                    {/* Curtain */}
+                    <volumeGroup
+                        ref={refCurtain}
+                        volumeClippingAutoUpdate={false}
+                    >
+                        <volumeAnimationObject
+                            ref={refCurtainAnimation}
+                            position={[45, 0, 48]}
+                            rotation={[0, Math.PI, -Math.PI / 2]}
+                        >
+                            <MODELS.Dose_curtain_all_Animation />
+                        </volumeAnimationObject>
+                        <Sphere position={[-50, 0, 0]} scale={25} />
+                    </volumeGroup>
+                    {/* Nocurtain */}
+                    <volumeGroup
+                        ref={refNocurtain}
+                        volumeClippingAutoUpdate={false}
+                    >
+                        <volumeAnimationObject
+                            ref={refNocurtainAnimation}
+                            position={[45, 0, 48]}
+                            rotation={[0, Math.PI, -Math.PI / 2]}
+                        >
+                            <MODELS.Dose_nocurtain_all_Animation />
+                        </volumeAnimationObject>
+                        <Box position={[50, 0, 0]} scale={25} />
+                    </volumeGroup>
+                </volumeGroup>
+
+                {/* Three.js Objects */}
+                <group rotation={[0, 0, Math.PI]} scale={1 / 4}>
+                    <MODELS.Dose_material />
+                    <MODELS.Dose_region />
+                </group>
+
+                {/* Contorls */}
+                <VolumeAnimationControls
+                    objects={[refCurtainAnimation, refNocurtainAnimation]}
+                    duration={16}
+                />
+                <VolumeParameterControls object={ref} />
+                {/* <VolumeClippingControls
+                    object={refCurtain}
+                    folderName="Dose"
+                    normals={[
+                        [0, 0, -1],
+                        // [-1, 0, 0],
+                    ]}
+                /> */}
+                <VolumeCompareControls
+                    object1={refCurtain}
+                    object2={refNocurtain}
+                    normals={[
+                        [0, 0, -1],
+                        // [-1, 0, 0],
+                    ]}
+                />
+
+                <ambientLight intensity={0.5} />
+                <OrthographicCamera />
+                <OrbitControls makeDefault />
+
+                <GizmoHelper
+                    alignment="bottom-right"
+                    margin={[80, 80]}
+                    renderPriority={-1}
+                >
+                    <GizmoViewport
+                        axisColors={["hotpink", "aquamarine", "#3498DB"]}
+                        labelColor="black"
+                    />
+                </GizmoHelper>
+            </Canvas>
+            <Stats />
+        </>
+    );
+}
+
+export default XRayCurtain;
--- components/scenes/index.tsx
+++ components/scenes/index.tsx
@@ -1,6 +1,5 @@
-export * from "../models/Dose/Dose_all_Animation";
-
-export { default as XRayRoom } from "./XRay_room";
-export { default as XRayRoomVR } from "./XRay_room_VR";
-export { default as XRayRoomGame } from "./XRay_room_Game";
 export { default as StentGame } from "./Stent_Game";
+export { default as XRayCurtain } from "./XRay_curtain";
+export { default as XRayRoomGame } from "./XRay_room_Game";
+export { default as XRayRoomVR } from "./XRay_room_VR";
+export { default as XRayRoom } from "./XRay_room";
