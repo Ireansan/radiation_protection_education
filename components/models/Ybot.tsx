ba95c5a0824fcb69ef922b88cac26581ca18c0ff
Ireansan <kkntp528@icloud.com>
2023-07-05 10:55:27 +0900

    add comment to rig structure

--- components/models/Ybot.tsx
+++ components/models/Ybot.tsx
@@ -61,3 +61,76 @@ export function YBot(props: JSX.IntrinsicElements["group"]) {
 }
 
 useGLTF.preload(modelURL);
+
+/*
+# Rig Structure
+
+```
+mixamorigHips
+    L mixamorigSpine
+    |   L mixamorigSpine1
+    |       L mixamorigSpine2
+    |           L mixamorigNeck
+    |           |   L mixamorigHead
+    |           |       L mixamorigHeadTop_End
+    |           L mixamorigLeftShoulder
+    |           |   L mixamorigLeftArm
+    |           |       L mixamorigLeftForeArm
+    |           |           L mixamorigLeftHand
+    |           |               L mixamorigLeftHandThumb1
+    |           |               |   L mixamorigLeftHandThumb2
+    |           |               |       L mixamorigLeftHandThumb3
+    |           |               |           L mixamorigLeftHandThumb4
+    |           |               L mixamorigLeftHandIndex1
+    |           |               |   L mixamorigLeftHandIndex2
+    |           |               |       L mixamorigLeftHandIndex3
+    |           |               |           L mixamorigLeftHandIndex4
+    |           |               L mixamorigLeftHandMiddle1
+    |           |               |   L mixamorigLeftHandMiddle2
+    |           |               |       L mixamorigLeftHandMiddle3
+    |           |               |           L mixamorigLeftHandMiddle4
+    |           |               L mixamorigLeftHandRing1
+    |           |               |   L mixamorigLeftHandRing2
+    |           |               |       L mixamorigLeftHandRing3
+    |           |               |           L mixamorigLeftHandRing4
+    |           |               L mixamorigLeftHandPinky1
+    |           |                   L mixamorigLeftHandPinky2
+    |           |                       L mixamorigLeftHandPinky3
+    |           |                           L mixamorigLeftHandPinky4
+    |           L mixamorigRightShoulder
+    |               L mixamorigRightArm
+    |                   L mixamorigRightForeArm
+    |                       L mixamorigRightHand
+    |                           L mixamorigRightHandThumb1
+    |                           |   L mixamorigRightHandThumb2
+    |                           |       L mixamorigRightHandThumb3
+    |                           |           L mixamorigRightHandThumb4
+    |                           L mixamorigRightHandIndex1
+    |                           |   L mixamorigRightHandIndex2
+    |                           |       L mixamorigRightHandIndex3
+    |                           |           L mixamorigRightHandIndex4
+    |                           L mixamorigRightHandMiddle1
+    |                           |   L mixamorigRightHandMiddle2
+    |                           |       L mixamorigRightHandMiddle3
+    |                           |           L mixamorigRightHandMiddle4
+    |                           L mixamorigRightHandRing1
+    |                           |   L mixamorigRightHandRing2
+    |                           |       L mixamorigRightHandRing3
+    |                           |           L mixamorigRightHandRing4
+    |                           L mixamorigRightHandPinky1
+    |                               L mixamorigRightHandPinky2
+    |                                   L mixamorigRightHandPinky3
+    |                                       L mixamorigRightHandPinky4
+    L mixamorigLeftUpLeg
+    |   L mixamorigLeftLeg
+    |       L mixamorigLeftFoot
+    |           L mixamorigLeftToeBase
+    |               L mixamorigLeftToe_End
+    L mixamorigRightUpLeg
+        L mixamorigRightLeg
+            L mixamorigRightFoot
+                L mixamorigRightToeBase
+                    L mixamorigRightToe_End
+```
+
+*/
