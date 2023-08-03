8757c73eefe6a130a30125e2500271155e4e93e3
Ireansan <kkntp528@icloud.com>
2023-08-03 11:04:09 +0900

    support new dose shader

--- src/volumeRender/core/doseObject.ts
+++ src/volumeRender/core/doseObject.ts
@@ -1,9 +1,13 @@
 import * as THREE from "three";
 import { Volume } from "three-stdlib";
 
+import doseShader from "../shaders/doseShader";
+import { cmtextures } from "../textures";
 import { VolumeObject } from "./volumeObject";
 
 class DoseObject extends VolumeObject {
+    _clippingPlanesIsBoard: boolean[];
+
     constructor(
         volume = new Volume(),
         coefficient = 1.0,
@@ -32,6 +36,90 @@ class DoseObject extends VolumeObject {
             clippingPlanes,
             clipIntersection
         );
+
+        this._clippingPlanesIsBoard = [];
+
+        // Texture
+        const texture = new THREE.Data3DTexture(
+            // @ts-ignore
+            this.volume.data,
+            this.width,
+            this.height,
+            this.depth
+        );
+        texture.format = THREE.RedFormat;
+        texture.type = THREE.FloatType;
+        texture.minFilter = texture.magFilter = THREE.LinearFilter;
+        texture.unpackAlignment = 1;
+        texture.needsUpdate = true;
+
+        // Material
+        const uniforms = THREE.UniformsUtils.clone(doseShader.uniforms);
+        uniforms.u_data.value = texture;
+        uniforms.u_size.value.set(this.width, this.height, this.depth);
+        uniforms.u_coefficient.value = this._coefficient;
+        uniforms.u_offset.value = this._offset;
+        uniforms.u_opacity.value = opacity;
+        uniforms.u_clim.value.set(this.clim1, this.clim2);
+        uniforms.u_renderstyle.value = this.renderstyle == "mip" ? 0 : 1; // 0: MIP, 1: ISO
+        uniforms.u_renderthreshold.value = this.isothreshold; // For ISO renderstyle
+        uniforms.u_cmdata.value = cmtextures[this.colormap];
+        this.updateMatrixWorld();
+        uniforms.u_modelMatrix.value = this.matrixWorld;
+
+        uniforms.u_clippedInitValue.value = this._clippedInitValue;
+        uniforms.u_clippingPlanesRegion.value = this._clippingPlanesRegion;
+        uniforms.u_clippingPlanesEnabled.value = this._clippingPlanesEnabled;
+        uniforms.u_clippingPlanesIsBoard.value = this._clippingPlanesIsBoard;
+        uniforms.u_clippedInvert.value = this._clippedInvert;
+
+        this.material = new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: doseShader.vertexShader,
+            fragmentShader: doseShader.fragmentShader,
+            side: THREE.BackSide, // The volume shader uses the backface as its "reference point"
+            transparent: true,
+            depthTest: false,
+            depthWrite: false,
+            clipping: clipping,
+            clippingPlanes: clippingPlanes,
+            clipIntersection: clipIntersection,
+        });
+    }
+
+    updateVolumeClipping(updateParents: boolean, updateChildren: boolean) {
+        // ----------
+        // update parent, this, and children
+        // ----------
+        super.updateVolumeClipping(updateParents, updateChildren);
+
+        // ----------
+        // reset this values
+        // ----------
+        this._clippingPlanesIsBoard = [];
+
+        // ----------
+        // update this
+        // ----------
+        for (let i = 0; i < this.totalClippingPlanesObjects.length; i++) {
+            let element = this.totalClippingPlanesObjects[i];
+
+            // Enabled
+            let isBoardArray = new Array(element.planes.length).fill(
+                element.isType === "board"
+            );
+            this._clippingPlanesIsBoard =
+                this._clippingPlanesIsBoard.concat(isBoardArray);
+        }
+        console.log(this._clippingPlanesIsBoard, this._coefficient);
+
+        // ----------
+        // update material
+        // ----------
+        this.material.uniforms.u_clippingPlanesIsBoard.value = this.material
+            .clipping
+            ? this._clippingPlanesIsBoard
+            : null;
     }
 
     getVolumeValue(position: THREE.Vector3): number {
