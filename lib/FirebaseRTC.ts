b51ea8e0dc9a15cb618fa4a753c7e726f6cf1245
Ireansan <kkntp528@icloud.com>
2023-09-26 22:41:07 +0900

    change firebase to firebase-admin

--- lib/FirebaseRTC.ts
+++ lib/FirebaseRTC.ts
@@ -4,11 +4,14 @@
  */
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"; // runs firebase side effects
+// import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"; // runs firebase side effects
+
+import { initializeApp } from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore"; // runs firebase side effects
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
