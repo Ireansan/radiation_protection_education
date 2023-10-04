9bd09d47c4a407c9bae8b7998bea4c8b8611a4d8
Ireansan <kkntp528@icloud.com>
2023-10-04 17:40:12 +0900

    add new Board Configure

---
+++ components/models/Environment/Machine/Board_Configure.ts
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+
+const Board_Configure = {
+    size: [0.57, 0.37, 0.02],
+};
+
+export { Board_Configure };
--- components/models/Environment/Machine/index.tsx
+++ components/models/Environment/Machine/index.tsx
@@ -1,2 +1,3 @@
 export * from "./Bed_rough";
+export * from "./Board_Configure";
 export * from "./C-Arm_rough";
