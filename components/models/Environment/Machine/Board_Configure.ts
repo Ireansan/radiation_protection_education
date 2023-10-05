d8f45b7587721976bfa5db8bd6d849f520d0722e
Ireansan <kkntp528@icloud.com>
2023-10-05 09:18:08 +0900

    change array to vector3, and add value for debug

--- components/models/Environment/Machine/Board_Configure.ts
+++ components/models/Environment/Machine/Board_Configure.ts
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 
 const Board_Configure = {
-    size: [0.57, 0.37, 0.02],
+    size: new THREE.Vector3(0.57, 0.37, 0.02),
+    // size: new THREE.Vector3(1, 2, 0.02), // for Debug
 };
 
 export { Board_Configure };
