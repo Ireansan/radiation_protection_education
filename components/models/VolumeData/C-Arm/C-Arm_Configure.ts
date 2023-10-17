e1d264308cf16698d09652ac59be0a6925bf9ea6
Ireansan <kkntp528@icloud.com>
2023-10-17 10:13:30 +0900

    update volume data configs

--- components/models/VolumeData/C-Arm/C-Arm_Configure.ts
+++ components/models/VolumeData/C-Arm/C-Arm_Configure.ts
@@ -1,15 +1,17 @@
 import * as THREE from "three";
+import type { CuboidArgs } from "@react-three/rapier";
 
 const CArm_Configure = {
     volume: {
-        position: [1.8, 0, -1.7] as THREE.Vector3Tuple, // world position
-        rotation: [0, 0, Math.PI / 2] as THREE.Vector3Tuple,
+        position: [-1.7, 0, -1.7] as THREE.Vector3Tuple, // world position
+        rotation: [0, Math.PI / 2, Math.PI / 2] as THREE.Vector3Tuple,
         scale: 1 / 19,
-        clim2: { timelapse: 1e-5, accumulate: 1 },
+        clim2: { timelapse: 1e-5, accumulate: 0.5e1 },
+        areaSize: [1.8, 1.4, 2] as CuboidArgs,
     },
     object3d: {
         position: [0, 1.3, 0] as THREE.Vector3Tuple, // world position
-        rotation: [0, 0, 0] as THREE.Vector3Tuple,
+        rotation: [0, Math.PI / 2, 0] as THREE.Vector3Tuple,
         scale: 1 / 4.5, // local scale
     },
 };
--- components/models/VolumeData/XRay_nocurtain/XRay_nocurtain_Configure.ts
+++ components/models/VolumeData/XRay_nocurtain/XRay_nocurtain_Configure.ts
@@ -1,10 +1,12 @@
 import * as THREE from "three";
+import type { CuboidArgs } from "@react-three/rapier";
 
 const XRay_nocurtain_Configure = {
     volume: {
         position: [2.1, 2.2, 2.35] as THREE.Vector3Tuple, // world position
         rotation: [0, Math.PI, -Math.PI / 2] as THREE.Vector3Tuple,
         scale: 1 / 19,
+        areaSize: [2.2, 1.2, 3.1] as CuboidArgs,
     },
     object3d: {
         position: [-0.195, 2.05, -0.19] as THREE.Vector3Tuple, // world position
