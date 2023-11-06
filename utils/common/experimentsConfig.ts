073d067176298984d096de7dc266079405a4bfb1
Ireansan <kkntp528@icloud.com>
2023-11-06 09:52:41 +0900

    update experimentsConfig

--- utils/common/experimentsConfig.ts
+++ utils/common/experimentsConfig.ts
@@ -3,4 +3,4 @@ import getConfig from "next/config";
 export const googleFormsURL =
     process.env.NODE_ENV === "development"
         ? "https://github.com/Ireansan/radiation_protection_education" // FIXME: Debug
-        : "https://forms.gle/pCSfzXUEjVyFn5yN7"; // FIXME: Debug
+        : "https://docs.google.com/forms/d/e/1FAIpQLScTz27uww9rpT1gusWK54hR1XBQiCeXpZ8QvELXWl-aauBFCw/viewform?usp=sf_link"; // FIXME: Debug
