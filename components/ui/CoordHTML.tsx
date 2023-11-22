8019816333197eefcefd63a4379e0c1fb8ad7cd4
Ireansan <48309811+Ireansan@users.noreply.github.com>
2023-11-22 10:54:30 +0900

    update Prototype CoordHTML

--- components/ui/CoordHTML.tsx
+++ components/ui/CoordHTML.tsx
@@ -5,26 +5,56 @@ import { Html } from "@react-three/drei";
 
 import styles from "../../styles/css/coordHtml.module.css";
 
-export function CoordHTML({ ...props }) {
-    const ref = React.useRef<THREE.Mesh>(null!);
+export type CoordHTMLProps = {
+    origin?: React.RefObject<THREE.Object3D> | THREE.Vector3;
+};
+export function CoordHTML({ origin, ...props }: CoordHTMLProps) {
+    const ref = React.useRef<THREE.Group>(null!);
+    const [position, setPosition] = React.useState<THREE.Vector3>(new THREE.Vector3());
+    const [rotation, setRotation] = React.useState<THREE.Vector3>(new THREE.Vector3());
+    const originWorldPosition = React.useMemo(() => {
+        const _position = new THREE.Vector3();
+        if (!origin) {
+            return _position;
+        }
+
+        if (origin instanceof THREE.Vector3) {
+            return origin;
+        } else if (origin.current) {
+            return _position.setFromMatrixPosition(origin.current.matrixWorld);
+        } else {
+            return _position;
+        }
+    }, [origin]);
 
     useFrame(() => {
-        console.log(
-            // new THREE.Vector3().setFromMatrixPosition(ref.current.matrixWorld)
-            new THREE.Euler().setFromRotationMatrix(ref.current.matrixWorld)
-        );
+        const _position = new THREE.Vector3().setFromMatrixPosition(ref.current.matrixWorld);
+        setPosition(_position);
+
+        const _rotationArray = new THREE.Vector3()
+            .setFromEuler(new THREE.Euler().setFromRotationMatrix(ref.current.matrixWorld))
+            .multiplyScalar(180.0 / Math.PI)
+            .toArray()
+            .map((value) => (value < 0 ? 360.0 + value : value));
+        const _rotation = new THREE.Vector3().fromArray(_rotationArray);
+        setRotation(_rotation);
     });
 
     return (
         <>
-            <mesh ref={ref}>
+            <group ref={ref}>
                 <Html distanceFactor={undefined}>
-                    <div className={`${styles.content}`}>
-                        hello <br />
-                        world
+                    <div className={`${styles.foundation}`}>
+                        <div className={`${styles.content}`}>
+                            {position.x.toFixed(1)}, {position.y.toFixed(1)},{" "}
+                            {position.z.toFixed(1)} <br />
+                            {rotation.x.toFixed(1)}, {rotation.y.toFixed(1)},{" "}
+                            {rotation.z.toFixed(1)} <br />
+                            {origin ? position.distanceTo(originWorldPosition) : ""}
+                        </div>
                     </div>
                 </Html>
-            </mesh>
+            </group>
         </>
     );
 }
