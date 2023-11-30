df2c12e4bb2ce8179b2f239697ea12257d4ecfa9
Ireansan <48309811+Ireansan@users.noreply.github.com>
2023-11-30 23:02:55 +0900

    install Swiper and add Tips

---
+++ components/ui/tips/Tips.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import style from "../../../styles/css/tips.module.css";
+
+/**
+ * @link https://swiperjs.com/demos#css-mode
+ */
+export function Tips() {
+    const test = [
+        "Slide 1",
+        "Slide 2",
+        "Slide 3",
+        "Slide 4",
+        "Slide 5",
+        "Slide 6",
+        "Slide 7",
+        "Slide 8",
+        "Slide 9",
+    ];
+
+    return (
+        <>
+            <div className={style.fullscreen}>
+                <div className={style.foundation}>
+                    <Swiper
+                        cssMode={true}
+                        navigation={true}
+                        pagination={true}
+                        mousewheel={true}
+                        keyboard={true}
+                        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                        className={style.swiper}
+                    >
+                        {test.map((value, index) => (
+                            <SwiperSlide
+                                key={index}
+                                className={style.swiperSlide}
+                            >
+                                {value}
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+        </>
+    );
+}
