940374f44faceb24e4b7c00501e5742bf36becd8
Ireansan <48309811+Ireansan@users.noreply.github.com>
2024-01-09 09:20:34 +0900

    update Google Form url

--- utils/common/experimentsConfig.ts
+++ utils/common/experimentsConfig.ts
@@ -2,5 +2,6 @@ import getConfig from "next/config";
 
 export const googleFormsURL =
     process.env.NODE_ENV === "development"
-        ? "https://github.com/Ireansan/radiation_protection_education" // FIXME: Debug
-        : ""; // FIXME: Debug
+        ? // ? "https://github.com/Ireansan/radiation_protection_education" // FIXME: Debug
+          "https://forms.gle/rARTkRysHuaLKKdZ7" // FIXME: Debug
+        : "https://forms.gle/rARTkRysHuaLKKdZ7";
