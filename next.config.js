9b5356b8529feba02b66c8cf3cc90b44b1182f6b
Ireansan <kkntp528@icloud.com>
2024-02-06 17:48:49 +0900

    uninstall mdx and skyway package

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-const withMDX = require("@next/mdx")({
-    extension: /\.mdx?$/,
-    options: {
-        // If you use remark-gfm, you'll need to use next.config.mjs
-        // as the package is ESM only
-        // https://github.com/remarkjs/remark-gfm#install
-        remarkPlugins: [],
-        rehypePlugins: [],
-        // If you use `MDXProvider`, uncomment the following line.
-        // providerImportSource: "@mdx-js/react",
-    },
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -32,4 +20,4 @@ const nextConfig = {
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
